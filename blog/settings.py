f806121e529e7562083afef3b4ebd3cee4a5fcba
Andrew <andrew.freelance@i.ua>
2013-10-25 10:17:24 +0300

    next commit with some little bugs...

--- blog/settings.py
+++ blog/settings.py
@@ -106,6 +106,9 @@ STATICFILES_FINDERS = (
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'social_auth.context_processors.social_auth_by_name_backends',
+    'social_auth.context_processors.social_auth_backends',
+    'social_auth.context_processors.social_auth_by_type_backends',
+    'social_auth.context_processors.social_auth_login_redirect',
     'django.contrib.auth.context_processors.auth',
 #    'django.core.context_processors.auth',
     'django.core.context_processors.media',
